dacc2f94fb589e8daf28ec2f01209450d960c0f2
Алишер Нурсаин <alisher.nursain@gmail.com>
2026-06-10 14:52:22 +0500

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { FadeIn } from './FadeIn';
+export { PageTransition } from './PageTransition';
+export { MagneticButton } from './MagneticButton';
+export { TextReveal } from './TextReveal';
+export type { } from './FadeIn';
